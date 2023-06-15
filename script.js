a5384fd3ff60632ed07b0a195621d88da590deee
dgdhruv <108942115+dgdhruv@users.noreply.github.com>
2023-06-15 16:56:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function nextPalindrome(num) {
-  //your JS code here.
+  //your JS code here.d
 }
 
 const input = prompt("Enter a palindrome number");
-alert(nextPalindrome(input));
+// alert(nextPalindrome(input));
